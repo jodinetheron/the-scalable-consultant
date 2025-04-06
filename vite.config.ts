7c24d43ca8d7ab56046ea39f694f6a8902dc798c
jodinetheron <jodinetheron@yahoo.com>
2025-04-06 23:33:10 +0200

    Configure for custom domain and update favicon/title

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/the-scalable-consultant",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
